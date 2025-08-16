5a9f1396c0a42e52d1725f7e8db2c5acf7836260
Alejandro Xavier Goncalves <alejandroxavier1121@gmail.com>
2025-08-16 15:57:03 -0300

    config: alias '@' con module-resolver + lib/supabase (web-safe)

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+// babel.config.js
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      ['module-resolver', {
+        root: ['./'],
+        alias: { '@': './' },
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+      }],
+    ],
+  };
+};
